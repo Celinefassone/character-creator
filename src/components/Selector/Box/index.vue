73d67f0ae597569c3a4e72f877fbbe94297a5fa2
Will Falkowski <willfalkowski@Wills-MacBook-Air.local>
2023-05-22 14:14:07 +0200

    Updated stuff

--- src/components/Selector/Box/index.vue
+++ src/components/Selector/Box/index.vue
@@ -1,15 +1,15 @@
 <template>
  <div class="selector"> 
-<button class="selector-box" @click="handleClick('/src/assets/wardrobe/head/hair.svg')" ref="button">
-    <img class="selector-box-hair" src="/src/assets/wardrobe/head/hair.svg" />
-  </button>
-  <button class="selector-box" @click="handleClick('/src/assets/wardrobe/chest/hoodie.svg')">
-    <img class="selector-box-hoodie" src="/src/assets/wardrobe/chest/hoodie.svg" />
-  </button>
-  <button class="selector-box"  @click="handleClick('/src/assets/wardrobe/legs/pants.svg')">
-    <img class="selector-box-pants" src="/src/assets/wardrobe/legs/pants.svg" />
-  </button>
-</div>
+  <button class="selector-box" @click="handleClick('/src/assets/wardrobe/head/hair.svg')" ref="button">
+      <img class="selector-box-hair" src="/src/assets/wardrobe/head/hair.svg" />
+    </button>
+    <button class="selector-box" @click="handleClick('/src/assets/wardrobe/chest/hoodie.svg')">
+      <img class="selector-box-hoodie" src="/src/assets/wardrobe/chest/hoodie.svg" />
+    </button>
+    <button class="selector-box"  @click="handleClick('/src/assets/wardrobe/legs/pants.svg')">
+      <img class="selector-box-pants" src="/src/assets/wardrobe/legs/pants.svg" />
+    </button>
+  </div>
 </template>
 
 <script>
