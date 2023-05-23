d6a64e6a96bbea93951563dd911787c26ddd29a2
Will Falkowski <will@digitalknight.io>
2023-05-23 11:01:34 +0200

    Button click stuff

--- src/components/Selector/Popupbox/index.vue
+++ src/components/Selector/Popupbox/index.vue
@@ -3,10 +3,10 @@ import { mapState } from 'vuex'
 </script>
 
 <template>
-<div v-show="isVisible" class="popup">
-  <button class="back-button" @click="handleClose"> ← </button>
-  <div class="popup-inside" v-if= "selection === 'head'">
-    <button class="popup-box">
+<div v-show="isVisible" class="popup" @click="handleClick">
+  <button class="back-button" @click.stop="handleClose"> ← </button>
+  <div class="popup-inside" v-if="selection === 'head'">
+  <button class="popup-box">
     <img class="popup-box-head" src="/images/wardrobe/head/diva.svg" />
   </button>
   <button class="popup-box">
@@ -19,7 +19,7 @@ import { mapState } from 'vuex'
     <img class="popup-box-head" src="/images/wardrobe/head/braids.svg" />
   </button>
   </div>
-  <div class="popup-inside" v-else-if= "selection === 'chest'">
+  <div class="popup-inside" v-else-if="selection === 'chest'">
     <button class="popup-box">
     <img class="popup-box-chest" src="/images/wardrobe/chest/dolly.svg" />
   </button>
@@ -36,7 +36,7 @@ import { mapState } from 'vuex'
     <img class="popup-box-chest" src="/images/wardrobe/chest/top.svg" />
   </button>
   </div>
-  <div class="popup-inside" v-else-if= "selection === 'legs'">
+  <div class="popup-inside" v-else-if="selection === 'legs'">
     <button class="popup-box">
     <img class="popup-box-legs" src="/images/wardrobe/legs/ballerina.svg" />
   </button>
@@ -66,6 +66,9 @@ export default {
   methods: {
     handleClose() {
       this.$store.commit('global/HIDE_POPUP')
+    },
+    handleClick(event) {
+      console.log(event.target.src)
     }
   }
 };
